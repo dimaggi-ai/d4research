ProdigyRahul/t3code

review round 2: correlated composer acks, stable queue order, projection consistency
- Correlate composer 'Sending' acknowledgment with the dispatched
  messageId (timeline or queue projection) instead of queue-tail
  heuristics that another client's queue activity could satisfy
  (Bugbot + Macroscope). Dispatches without a known messageId keep the
  legacy latest-user-message heuristic.
- Drain queued messages in rowid (insertion) order instead of
  queued_at + message_id, which mis-ordered same-timestamp messages
  after a restart (Macroscope). Requeue replaces the row so an edited
  messageId moves to the tail, matching the in-memory projector.
- Clear queuedMessages in the in-memory projector on thread.deleted,
  mirroring the SQL projection (Bugbot).
- Bootstrap the queuedMessages projector before threadMessages so
  revert pruning sees queued attachments during replay (Macroscope).
- Allow steer while a session reports 'starting' with a live
  activeTurnId (provider reconnect mid-turn); only the pending-start
  shape (starting, no active turn) is rejected (Bugbot).
- Settle mock provider sessions in ProviderCommandReactor tests whose
  follow-up sends now queue-by-default against a still-running session.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

apps/server/src/orchestration/Layers/ProviderCommandReactor.test.ts
@@ -418,9 +418,39 @@ describe("ProviderCommandReactor", () => {
       }),
     );
 
+    // Queue-by-default holds turn.start commands while the session is
+    // starting/running. The mock provider never emits runtime events, so
+    // tests that send a follow-up on an idle thread must settle the session
+    // back to ready first — in production, turn completion does this. The
+    // projected session is preserved apart from status/activeTurnId so
+    // provider identity fields survive the settle.
+    let settleIndex = 0;
+    const settleSession = async (threadId: ThreadId = ThreadId.make("thread-1")) => {
+      settleIndex += 1;
+      const readModel = await Effect.runPromise(snapshotQuery.getSnapshot());
+      const session = readModel.threads.find((entry) => entry.id === threadId)?.session;
+      if (!session) {
+        return;
+      }
+      await Effect.runPromise(
+        engine.dispatch({
+          type: "thread.session.set",
+          commandId: CommandId.make(`cmd-session-settle-${settleIndex}`),
+          threadId,
+          session: {
+            ...session,
+            status: "ready",
+            activeTurnId: null,
+          },
+          createdAt: now,
+        }),
+      );
+    };
+
     return {
       engine,
       readModel: () => Effect.runPromise(snapshotQuery.getSnapshot()),
+      settleSession,
       startSession,
       sendTurn,
       interruptTurn,
@@ -985,6 +1015,7 @@ describe("ProviderCommandReactor", () => {
 
     await waitFor(() => harness.sendTurn.mock.calls.length === 1);
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",
@@ -1039,6 +1070,7 @@ describe("ProviderCommandReactor", () => {
 
         yield* Effect.promise(() => waitFor(() => harness.sendTurn.mock.calls.length === 1));
 
+        yield* Effect.promise(() => harness.settleSession());
         yield* harness.engine.dispatch({
           type: "thread.turn.start",
           commandId: CommandId.make("cmd-turn-start-restricted-2"),
@@ -1159,6 +1191,7 @@ describe("ProviderCommandReactor", () => {
     await waitFor(() => harness.startSession.mock.calls.length === 1);
     await waitFor(() => harness.sendTurn.mock.calls.length === 1);
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",
@@ -1208,6 +1241,7 @@ describe("ProviderCommandReactor", () => {
 
     await waitFor(() => harness.sendTurn.mock.calls.length === 1);
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",
@@ -1284,6 +1318,7 @@ describe("ProviderCommandReactor", () => {
       }),
     );
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",
@@ -1350,6 +1385,7 @@ describe("ProviderCommandReactor", () => {
     await waitFor(() => harness.startSession.mock.calls.length === 1);
     await waitFor(() => harness.sendTurn.mock.calls.length === 1);
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",
@@ -1606,6 +1642,7 @@ describe("ProviderCommandReactor", () => {
     await waitFor(() => harness.startSession.mock.calls.length === 1);
     await waitFor(() => harness.sendTurn.mock.calls.length === 1);
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",

dimaggi-ai/d4research

apps/server/src/orchestration/Layers/ProviderCommandReactor.test.ts
@@ -746,9 +746,39 @@ describe("ProviderCommandReactor", () => {
     await Effect.runPromise(reactor.start().pipe(Scope.provide(scope)));
     const drain = () => Effect.runPromise(reactor.drain);
 
+    // Queue-by-default holds turn.start commands while the session is
+    // starting/running. The mock provider never emits runtime events, so
+    // tests that send a follow-up on an idle thread must settle the session
+    // back to ready first — in production, turn completion does this. The
+    // projected session is preserved apart from status/activeTurnId so
+    // provider identity fields survive the settle.
+    let settleIndex = 0;
+    const settleSession = async (threadId: ThreadId = ThreadId.make("thread-1")) => {
+      settleIndex += 1;
+      const readModel = await runtime.runPromise(snapshotQuery.getSnapshot());
+      const session = readModel.threads.find((entry) => entry.id === threadId)?.session;
+      if (!session) {
+        return;
+      }
+      await runtime.runPromise(
+        engine.dispatch({
+          type: "thread.session.set",
+          commandId: CommandId.make(`cmd-session-settle-${settleIndex}`),
+          threadId,
+          session: {
+            ...session,
+            status: "ready",
+            activeTurnId: null,
+          },
+          createdAt: now,
+        }),
+      );
+    };
+
     return {
       engine,
       readModel: () => Effect.runPromise(snapshotQuery.getSnapshot()),
+      settleSession,
       startSession,
       sendTurn,
       interruptTurn,
@@ -2684,6 +2714,7 @@ describe("ProviderCommandReactor", () => {
 
     await waitFor(() => harness.sendTurn.mock.calls.length === 1);
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",
@@ -2738,6 +2769,7 @@ describe("ProviderCommandReactor", () => {
 
         yield* Effect.promise(() => waitFor(() => harness.sendTurn.mock.calls.length === 1));
 
+        yield* Effect.promise(() => harness.settleSession());
         yield* harness.engine.dispatch({
           type: "thread.turn.start",
           commandId: CommandId.make("cmd-turn-start-restricted-2"),
@@ -2858,6 +2890,7 @@ describe("ProviderCommandReactor", () => {
     await waitFor(() => harness.startSession.mock.calls.length === 1);
     await waitFor(() => harness.sendTurn.mock.calls.length === 1);
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",
@@ -2907,6 +2940,7 @@ describe("ProviderCommandReactor", () => {
 
     await waitFor(() => harness.sendTurn.mock.calls.length === 1);
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",
@@ -2983,6 +3017,7 @@ describe("ProviderCommandReactor", () => {
       }),
     );
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",
@@ -3049,6 +3084,7 @@ describe("ProviderCommandReactor", () => {
     await waitFor(() => harness.startSession.mock.calls.length === 1);
     await waitFor(() => harness.sendTurn.mock.calls.length === 1);
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",
@@ -3305,6 +3341,7 @@ describe("ProviderCommandReactor", () => {
     await waitFor(() => harness.startSession.mock.calls.length === 1);
     await waitFor(() => harness.sendTurn.mock.calls.length === 1);
 
+    await harness.settleSession();
     await Effect.runPromise(
       harness.engine.dispatch({
         type: "thread.turn.start",